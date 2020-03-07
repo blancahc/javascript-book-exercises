1f78c61626b225c77ae245c7d7bba5986c4d4c49
Blanca Hernandez <blamca1029@gmail.com>
2020-03-06 23:41:05 -0800

    completed preparations excercises

---
+++ javascript-exercises/preparations_exercises/foo.js
@@ -0,0 +1,57 @@
+var foo = 'bar';
+console.log(foo);
+foo;
+
+// Exercise 5: 
+// substring  - String/ prototype
+// create - Object /static
+// fromCharCode -String/ static 
+// slice - Array,String/ prototype
+
+// 6. Which of the following names satisfy the style guidelines for variable names?
+
+// Name - no
+// index - yes
+// CatName - no
+// snake_case - no
+// lazyDog - yes
+// quick_Fox - no
+// 1stCharacter - yes x
+// operand2 - yes
+// BIG_NUMBER - yes x
+
+// 7. Which of the following names satisfy the style guidelines for function names?
+
+// Name - if constructor x
+// index - yes
+// CatName - if constructor
+// snake_case - no
+// lazyDog - yes
+// quick_Fox - no
+// 1stCharacter - no
+// operand2 - yes
+// BIG_NUMBER - no
+
+// 8. Which of the following names satisfy the style guidelines for constants?
+
+// Name 
+// index
+// CatName
+// snake_case
+// lazyDog
+// quick_Fox
+// 1stCharacter
+// operand2
+// BIG_NUMBER - this one
+
+// 9. Which of the following names don't satisfy the style guidelines for naming variables, functions, or constants?
+
+// Name
+// index
+// CatName
+// snake_case - this
+// lazyDog
+// quick_Fox - this
+// 1stCharacter - this
+// operand2
+// BIG_NUMBER
